3a0fc098496dc66e67f5ce0f7d5bcfe7e2385eea
Alex Buccheri <alexanderbuccheri@googlemail.com>
2024-08-21 13:17:14 +0100

    Refactor to return results in a table.

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 from typing import Optional
 
+import pandas as pd
 import streamlit as st
 import yaml
 
+"""
+Drug  Concentration (mg/ml)  
+"""
 
-def volume_dose(database, name: str, weight: float, dose: Optional[float] = None) -> float:
+
+def volume_dose(database, name: str, weight: float, dose: float) -> float:
     """ Return the total volume of a drug dosage to administer, given the weight of the dog and
     the drug concentration, and dose per kg.
 
     :param database: Dict of default drug doses and concentrations
     :param name: Drug name
     :param weight: Dog weight (kg)
-    :param dose: Optional drug dose (mg/kg). If not passed, the default is used.
+    :param dose: Drug dose (mg/kg)
     :return: Drug dosage volume, in ml
     """
     if name not in database.keys():
         raise ValueError(f'Drug {name} is not currently in the database')
-
-    if not dose:
-        dose = database[name]['dose']
-        st.write(f'Using the default dose for {name} of {dose} (ml/kg)')
 
     total_dose = dose * weight
     return total_dose / database[name]['concentration']
@@ -37,13 +38,29 @@ def main():
 
     dose = {}
     for name in selected_drugs:
-        dose[name] = st.number_input(f"Enter the dose (ml per kg) for {name}."
-                                     f"If no value is entered, the default is used:", min_value=0.0, step=0.1, key=name)
+        dose[name] = st.number_input(f"Enter the dose (ml per kg) for {name}. "
+                                     "If no value is entered, the default is used:", min_value=0.0, step=0.1, key=name)
 
+    df_contents = []
     if st.button("Calculate"):
         for name in selected_drugs:
-            result = volume_dose(drug_database, name, weight_kg, dose[name])
-            st.write(f'Administer {result} (ml) for {name}')
+
+            if dose[name] < 1.e-8:
+                dose[name] = drug_database[name]['dose']
+                st.write(f'Using the default dose for {name} of {dose[name]} (ml/kg)')
+
+            vol = volume_dose(drug_database, name, weight_kg, dose[name])
+            # st.write(f'Administer {vol} (ml) for {name}')
+
+            # Package for table view in ST
+            df_contents.append(
+                {'Drug': name,
+                 'Concentration (mg/ml)': drug_database[name]['concentration'],
+                 'Dose (mg/kg)': dose[name],
+                 'Volume to Give (ml)': vol}
+            )
+
+        st.dataframe(pd.DataFrame(df_contents), use_container_width=True)
 
 
 if __name__ == "__main__":
